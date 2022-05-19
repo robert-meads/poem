84eaa8230d26ec0603177c052ab8918cf9410401
louDack <76420417+louDack@users.noreply.github.com>
2022-05-19 13:09:20 -0700

    Stupid new branch button.
    Why is it hidden???

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 <p>This is the life of slim.!</p>
 
 <p>This is louDack posting from forked repo!</p>
+<p>Hiding the new branch button. Stupid!. You can only create a new branch when you commit.</p>
